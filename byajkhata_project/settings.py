fc68f322d1e324db688665fb2589919a0f68540b
Shivam Patidar <shivampatidar@Shivams-MacBook-Air.local>
2025-10-29 18:39:17 +0530

    Fix CORS settings

--- byajkhata_project/settings.py
+++ byajkhata_project/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'widget_tweaks',
     'core',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,8 +52,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:58892",
+    "http://127.0.0.1:58892",
+    "https://byajkhata.onrender.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://byajkhata.onrender.com",
+    "https://byajkhata-backend.onrender.com",
+]
 
 TEMPLATES = [
     {
